8bb5d4837c6cfc2856e50ac6ba04a0a30985cbd2
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-03-10 12:25:31 +0700

    add favicon, and CRUD Promo page.

--- app/promo/controller.js
+++ app/promo/controller.js
@@ -2,14 +2,65 @@ const Promo = require('./model');
 
 module.exports = {
     index: async (req, res) => {
-        res.render('promo/index');
+        await Promo.find({})
+            .then(r => {
+                res.render('promo/index', { r });
+            })
+            .catch(e => {
+                console.log(e);
+                res.redirect('/');
+            })
     },
     viewAdd: async (req, res) => {
         res.render('promo/viewAdd');
     },
+    actionAdd: async (req, res) => {
+        const { code, name, discountValue, status, startDate, expiryDate } = req.body
+
+        await Promo.create({ code, name, discountValue, status, startDate, expiryDate })
+            .then(r => {
+                res.redirect('/promo')
+            })
+            .catch(e => {
+                console.log(e);
+                res.redirect('/');
+            })
+    },
     viewDetail: async (req, res) => {
         const { _id } = req.params;
 
-        res.render('promo/viewDetail', { _id });
+        await Promo.findById(_id)
+            .then(r => {
+                res.render('promo/viewDetail', { r });
+            })
+            .catch(e => {
+                console.log(e);
+                res.redirect('/');
+            })
+    },
+    actionUpdate: async (req, res) => {
+        const { _id } = req.params;
+        const { name, discountValue, status, startDate, expiryDate } = req.body
+
+        await Promo.findByIdAndUpdate(_id, { name, discountValue, status, startDate, expiryDate })
+            .then(r => {
+                res.redirect('/promo')
+            })
+            .catch(e => {
+                console.log(e);
+                res.redirect('/');
+            })
+    },
+    actionDelete: async (req, res) => {
+        const { _id } = req.params;
+
+        await Promo.findByIdAndDelete(_id)
+            .then(r => {
+                res.redirect('/promo')
+            })
+            .catch(e => {
+                console.log(e);
+                res.redirect('/');
+            })
     }
 }
